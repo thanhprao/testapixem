d6c916e05e03d0ee70bba6724ca27f54cb451f72
Nguyen Thanh <73066048+thanhprao@users.noreply.github.com>
2023-06-30 17:40:22 +0700

    Update extract.py

--- extract.py
+++ extract.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 def createDriver() -> webdriver.Chrome:
     chrome_options = webdriver.ChromeOptions()
     
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     
